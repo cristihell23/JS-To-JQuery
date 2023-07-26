1e9b542c3d46a085c5da837bc3ed1c7fb30ca3e8
Cristian Hanganu <cristihell23@yahoo.com>
2023-07-26 23:02:22 +0000

    basic Jquery commands

--- 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
+++ 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-    
+    $('h2').addClass('underline') // will change the text-decoration to underline. Can use .removeClass
+    $('ul').addClass('border'); // add border to all ul. Can use .removeClass
 });
